3142142d5976a64f3664619d30e95dfba690d972
Michael Ingold <mike.ingold@gmail.com>
2025-06-24 07:07:42 -0400

    Extend supports_autoenzyme to Domains

--- ext/MeshIntegralsEnzymeExt.jl
+++ ext/MeshIntegralsEnzymeExt.jl
@@ -18,7 +18,7 @@ end
 
 # Supports all geometries except for those that throw errors
 # See GitHub Issue #154 for more information
-MeshIntegrals.supports_autoenzyme(::Type{<:Meshes.Geometry}) = true
+MeshIntegrals.supports_autoenzyme(::Type{<:Meshes.GeometryOrDomain}) = true
 MeshIntegrals.supports_autoenzyme(::Type{<:Meshes.BezierCurve}) = false
 MeshIntegrals.supports_autoenzyme(::Type{<:Meshes.CylinderSurface}) = false
 MeshIntegrals.supports_autoenzyme(::Type{<:Meshes.Cylinder}) = false
